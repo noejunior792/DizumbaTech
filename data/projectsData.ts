c81dfb666b789605cf3df39ecf11e1816f7a6d06
IgorSemed0 <semedoigor4@gmail.com>
2024-11-17 14:10:11 +0100

    Version 1.0

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -21,15 +21,15 @@ const projectsData: Project[] = [
     a decidir qual é o melhor para suas necessidades.`,
     imgSrc: '/static/images/publicacoes/projectos/rinha-so-linux-vs-windows.jpg',
     href: '/blog/rinha-so-linux-vs-windos/rinha-so-linux-vs-windows', 
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+  }
+  // {
+  //   title: 'The Time Machine',
+  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+  //   to the desired date and press "Go". No more worrying about lost keys or
+  //   forgotten headphones with this simple yet affordable solution.`,
+  //   imgSrc: '/static/images/time-machine.jpg',
+  //   href: '/blog/the-time-machine',
+  // },
 ]
 
 export default projectsData
